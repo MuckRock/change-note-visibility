efe24193c16a942918dfbe480fb7b0b0a7163c62
s <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-20 10:32:33 -0500

    Ran pylint and black

--- main.py
+++ main.py
@@ -2,24 +2,26 @@
 This is an Add-On that allows you to change the visibility of all notes queried or selected.
 """
 import sys
-from documentcloud.addon import AddOn, SoftTimeOutAddOn
+from documentcloud.addon import SoftTimeOutAddOn
+
 
 class ChangeNoteVisibility(SoftTimeOutAddOn):
     """Bulk changes visibility of notes on documents selected."""
 
     def main(self):
-        """For all of the documents selected it will change the visibility of the notes 
-           on these documents to the one specified."""
+        """For all of the documents selected it will change the visibility of the notes
+        on these documents to the one specified."""
         # fetch the access_level specified
         access_level = self.data["access_level"]
-        accepted_values = ['private', 'public', 'organization']
+        accepted_values = ["private", "public", "organization"]
         if access_level not in accepted_values:
-            self.set_message("You set an invalid access level- must be private, public, or organization")
+            self.set_message("You set an invalid access level. Try again.")
             sys.exit(1)
         for document in self.get_documents():
             for note in document.notes:
                 note.access = access_level
                 note.save()
+
 
 if __name__ == "__main__":
     ChangeNoteVisibility().main()
